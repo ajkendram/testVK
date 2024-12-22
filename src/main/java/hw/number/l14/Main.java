77c0b1e9efad83dae3de8da9e37b8cd3b4bb198a
v.k <ajkendram@gmail.com>
2024-12-22 16:48:40 +0300

    14

---
+++ src/main/java/hw/number/l14/Main.java
@@ -0,0 +1,23 @@
+package hw.number.l14;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        int fatorial = 1;
+        int number = 5;
+
+        for (int i = 1; i <= number; i++){
+
+
+            fatorial = fatorial * i;
+
+
+        }
+
+
+        System.out.println(fatorial);
+
+    }
+}
