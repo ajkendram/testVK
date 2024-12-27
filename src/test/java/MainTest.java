fda92ea5d1f063f4159546b2ca448078ee3f9a19
vk <ajkendram@gmail.com>
2024-12-27 20:32:57 +0300

    15 1

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,34 +1,96 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 public class MainTest {
 
-    public static void main(String[] args) {
+    private WebDriver driver;
+    private PageHomeMTS pageHomeMTS;
 
-        int number = 5;
-        int factorial = calculateFactorial(number);
-        System.out.println(factorial);
+
+
+
+    @BeforeEach
+    public void setup() {
+        driver = new ChromeDriver();
+        pageHomeMTS = new PageHomeMTS(driver);
+
+        driver.get("https://www.mts.by/");
+        driver.manage().window().maximize();
+
+        try {
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+            wait.until(ExpectedConditions.elementToBeClickable(pageHomeMTS.getAcceptButton()));
+            pageHomeMTS.clickControl(pageHomeMTS.acceptButton);
+        } catch (Exception e) {
+            System.out.println("Failed to click the accept button: " + e.getMessage());
+        }
     }
 
-    public static int calculateFactorial(int number) {
-        int factorial = 1;
-        for (int i = 1; i <= number; i++) {
-            factorial *= i;
-        }
-        return factorial;
+
+    @Test
+    public void testBlockName() {
+        //Assert.assertEquals();
+        //System.out.println("111111111111111");
+        String expectedText = "Онлайн пополнение без комиссии";
+        String actualText = pageHomeMTS.getHeaderText();
+        assertEquals(expectedText, actualText);
+    }
+
+
+    @Test
+    public void testVisaIcon() {
+        assertTrue(pageHomeMTS.isVisaImagePresentForAll(pageHomeMTS.visaImage), "No Visa");
     }
 
     @Test
-    public void testCalculateFactorial() {
-        int number = 5;
-        int expectedFactorial = 120;
-        int actualFactorial = calculateFactorial(number);
-        Assert.assertEquals(actualFactorial, expectedFactorial);
-    }
+    public void testIconAll() {
+
+        //для упрощения все в одном тесте
+        assertTrue(pageHomeMTS.isVisaImagePresentForAll(pageHomeMTS.mastercardImage), "No mastercardImage");
+        assertTrue(pageHomeMTS.isVisaImagePresentForAll(pageHomeMTS.mastercardSecureImage), "No mastercardSecureImage");
+        assertTrue(pageHomeMTS.isVisaImagePresentForAll(pageHomeMTS.belImage), "No belImage");
+        assertTrue(pageHomeMTS.isVisaImagePresentForAll(pageHomeMTS.visaImageVer), "No visaImageVer");
 
     }
 
 
+    @Test
+    public void testDetailsLink() {
+        String initialUrl = driver.getCurrentUrl();
+        pageHomeMTS.clickControl(pageHomeMTS.detailsLink);
 
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.not(ExpectedConditions.urlToBe(initialUrl)));
+
+        String newUrl = driver.getCurrentUrl();
+        assertTrue(!newUrl.equals(initialUrl), "The page URL should change");
+    }
+
+
+
+    @AfterEach
+    public void tearDown() {
+            driver.quit();
+
+
+
+    }
+
+
+
+}
